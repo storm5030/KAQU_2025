1c095e113babc7cc3f5b52bc9f1ed2657014c416
storm5030 <storm5030@gmail.com>
2025-08-23 15:23:51 +0900

    Add Comments

--- kaqu_controller/kaqu_controller/KaquCmdManager/TorquePublisher.py
+++ kaqu_controller/kaqu_controller/KaquCmdManager/TorquePublisher.py
@@ -8,13 +8,14 @@ from std_msgs.msg import Float32MultiArray
 
 class TorquePublisher(Node):
     """
-    Wrench 토픽 구독 (힘/토크값 - 토크맨 사용)
-    각 토크 벡터를 관절축 기준으로 계산하여 퍼블리시
+    Wrench 토픽 구독
+    각 토크를 병합하여 퍼블리시
     """
 
     def __init__(self):
         super().__init__('torque_publisher')
 
+        # 4개 관절의 토크값 섭스크라이브 (세팅)
         self.topics = [
             '/force_torque/fl_14',
             '/force_torque/fr_14',
@@ -22,21 +23,27 @@ class TorquePublisher(Node):
             '/force_torque/rr_14',
         ]
 
+        # 퍼블리시용 배열
         self.tau_x = [0.0, 0.0, 0.0, 0.0]
         self.valid = [False, False, False, False]
 
+        # 퍼블리셔 선언
         self.publisher = self.create_publisher(Float32MultiArray, '/torque_14', 10)
         self.subs = []
+
+        # 4개 관절로부터 섭스크라이브
         for i, t in enumerate(self.topics):
-            sub = self.create_subscription(Wrench, t, lambda msg, idx=i: self.cb(msg, idx), 50)
+            sub = self.create_subscription(Wrench, t, lambda msg, idx=i: self.callback(msg, idx), 50)
             self.subs.append(sub)
             self.get_logger().info(f'[Sub {i}] {t} (Wrench)')
         self.get_logger().info('[Pub] /torque_14 (Float32MultiArray[4])')
 
-    def cb(self, msg: Wrench, idx: int):
+    # 섭스크라이브 콜백함수, 조인트 개수만큼 호출됨
+    def callback(self, msg: Wrench, idx: int):
         self.tau_x[idx] = float(msg.torque.x)
         self.valid[idx] = True
         
+        # 모든 값이 들어왔을 때만 퍼블리시
         if all(self.valid):
             out = Float32MultiArray()
             out.data = list(self.tau_x)
