badf292b7a7bed09e4801d7f2b282919c3ea391a
Woojin <kimwoojin.alex@gmail.com>
2025-08-25 22:50:29 +0900

    WIP: temporary commit for branch change

--- kaqu_llm/navigate.py
+++ kaqu_llm/navigate.py
@@ -1,1 +1,30 @@
-# 여기에 구현해보세요
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Point
+import json
+
+
+with open("map.json", "r", encoding="utf-8") as f:
+    map_data = json.load(f)
+
+nodes = map_data["nodes"]
+edges = map_data["edges"]
+
+
+
+class TestNode(Node):
+    def __init__(self):
+        super().__init__('test_node')
+        self.get_logger().info('✅ TestNode has started successfully!')
+
+def main(args=None):
+    rclpy.init(args=args)
+    # node = Navigator()
+    node = TestNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
