24a28ca8b6d5a89e2b722b98fc5d869914fc0752
kino-works <realkinomail@gmail.com>
2025-09-01 23:25:18 +0900

    env 수정

--- kaqu_llm_ina/setup.py
+++ kaqu_llm_ina/setup.py
@@ -9,8 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'map.json']),
-        ('share/' + package_name, ['.env']),
+        ('share/' + package_name, ['package.xml', 'map.json'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
